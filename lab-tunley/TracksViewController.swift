3c265f53b2e3bdc5af0a11127567f429cf7515a2
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-08 14:41:49 -0800

    Setup table view

--- lab-tunley/TracksViewController.swift
+++ lab-tunley/TracksViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class TracksViewController: UIViewController {
+class TracksViewController: UIViewController, UITableViewDataSource {
 
     // TODO: Pt 1 - Add a tracks property
     var tracks: [Track] = []
 
     // TODO: Pt 1 - Add table view outlet
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
         // TODO: Pt 1 - Set tracks property with mock tracks array
         tracks = Track.mockTracks
@@ -30,6 +33,22 @@ class TracksViewController: UIViewController {
     }
 
     // TODO: Pt 1 - Add table view data source methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell with identifier, "TrackCell"
+        // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
 
+        // Get the track that corresponds to the table view row
+        let track = tracks[indexPath.row]
 
+        // Configure the cell with it's associated track
+        cell.configure(with: track)
+
+        // return the cell for display in the table view
+        return cell
+    }
 }
